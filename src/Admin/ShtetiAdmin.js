763f623b8628b91d4338fa489ac9a11db6300eaf
anda <ap56170@ubt-uni.net>
2024-01-14 20:43:02 -0800

    ShtetiAdmin

---
+++ src/Admin/ShtetiAdmin.js
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
+
+function Shtetet() {
+  const [id, setId] = useState("");
+  const [emri, setEmri] = useState("");
+  const [shtetet, setShtetet] = useState([]);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
+
+  useEffect(() => {
+    (async () => await Load())();
+  }, []);
+
+  async function Load() {
+    try {
+      const result = await axios.get(
+        "https://localhost:7239/api/Shteti/GetAllList"
+      );
+      setShtetet(result.data);
+      console.log(result.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function save(event) {
+    event.preventDefault();
+    try {
+      await axios.post("https://localhost:7239/api/Shteti/Add", {
+        emri: emri,
+      });
+      showAndHideAlert("Shteti është regjistruar me sukses!");
+      setId("");
+      setEmri("");
+      Load();
+    } catch (err) {
+      showAndHideAlert(`Error: ${err}`, true);
+    }
+  }
+
+  async function editShteti(shteti) {
+    setEmri(shteti.emri);
+    setId(shteti.id);
+  }
+
+  async function deleteShteti(id) {
+    try {
+      await axios.delete(`https://localhost:7239/api/Shteti/Delete?Id=${id}`);
+      showAndHideAlert("Shteti është fshir me sukses!");
+      setId("");
+      setEmri("");
+      Load();
+    } catch (err) {
+      showAndHideAlert(`Error: ${err}`, true);
+    }
+  }
+
+  async function update(event) {
+    event.preventDefault();
+    try {
+      const shteti = shtetet.find((p) => p.id === id);
+      await axios.put(`https://localhost:7239/api/Shteti/Update/${shteti.id}`, {
+        id: shteti.id,
+        emri: emri,
+      });
+      showAndHideAlert("Shteti është edituar me sukses!");
+      setId("");
+      setEmri("");
+      Load();
+    } catch (err) {
+      showAndHideAlert(`Error: ${err}`, true);
+    }
+  }
+
+  function showAndHideAlert(message, isError = false) {
+    setAlertMessage(message);
+    setIsAlertVisible(true);
+
+    setTimeout(() => {
+      setIsAlertVisible(false);
+    }, 3000); // Hide the alert after 3 seconds
+  }
+
+  ///////////////////////////////////////////////////////////////
+  return (
+    <div>
+      <h4 className="description">Të dhënat për Shtetin</h4>
+      <div className="container mt-4">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="id"
+              hidden
+              value={id}
+              onChange={(event) => {
+                setId(event.target.value);
+              }}
+            />
+
+            <label className="label">Emri</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={emri}
+              onChange={(event) => {
+                setEmri(event.target.value);
+              }}
+            />
+          </div>
+
+          <div>
+            <button className="btn btn-success m-4 button" onClick={save}>
+              Save
+            </button>
+            <button className="btn btn-warning m-4 button" onClick={update}>
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+      <br></br>
+
+      {/* Alert Message */}
+      {isAlertVisible && (
+        <div
+          className={`alert ${
+            alertMessage.includes("Error") ? "alert-danger" : "alert-success"
+          }`}
+        >
+          {alertMessage}
+        </div>
+      )}
+
+      <div className="table-responsive m-3">
+        <table className="table border-gray">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Emri</th>
+
+              <th scope="col">Opsionet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {shtetet.map(function fn(shteti) {
+              return (
+                <tr key={shteti.id}>
+                  <td>{shteti.id}</td>
+                  <td>{shteti.emri}</td>
+
+                  <td className="d-flex align-items-center button-container ">
+                    <button
+                      type="button"
+                      className="btn btn-warning mx-1 button"
+                      onClick={() => editShteti(shteti)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger mx-1 button"
+                      onClick={() => deleteShteti(shteti.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Shtetet;
