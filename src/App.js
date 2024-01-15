097aa534f076b85bee36f97d6391eff2d415ef81
anda <ap56170@ubt-uni.net>
2024-01-14 20:47:50 -0800

    Rregullimi App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Dashboard from "./Admin/Dashboard";
 import Sidebar from "./Admin/Sidebar";
+import Nav from "./Admin/Nav";
+import Home from "./Admin/Home";
+import Shtetet from "./Admin/ShtetiAdmin";
+import Qytetet from "./Admin/QytetiAdmin";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
+  const Toggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
+    <Router>
+      <div
+        className="container-fluid"
+        style={{
+          backgroundColor: "#004687",
+          minHeight: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="row">
+          {toggle && (
+            <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+              <Sidebar />
+            </div>
+          )}
+          {toggle && <div className="col-4 col-md-2"></div>}
+          <div className="col">
+            <Nav Toggle={Toggle} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/home" element={<Home />} />
+              {/* Perdoruesit url*/}
+              <Route path="/shtetet" element={<Shtetet />} />
+              <Route path="/qytetet" element={<Qytetet />} />
+              {/* Aeroplanet url*/}
+              {/* Fluturimet url*/}
+              {/* Rezervimet url*/}
+              {/* Mesazhet url*/}
+
+              {/* Logout url*/}
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
