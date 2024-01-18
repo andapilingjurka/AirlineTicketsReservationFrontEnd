5b695ad673e7401e78b80b2d5a12d6424b1d2379
Dea <dk61403@ubt-uni.net>
2024-01-19 00:50:42 +0100

    Aeroplani Pjesa Return

--- src/Admin/AeroplaniAdmin.js
+++ src/Admin/AeroplaniAdmin.js
@@ -114,7 +114,154 @@ function Aeroplanet() {
 
   ///////////////////////////////////////////////////////////////
   return (
-   
+    <div
+    className="container-fluid"
+    style={{
+      backgroundColor: "#004687",
+      minHeight: "100vh",
+      backgroundSize: "cover",
+    }}
+  >
+    <div className="row">
+      {toggle && (
+        <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+          <Sidebar />
+        </div>
+      )}
+
+      <div className="col-4 col-md-2"></div>
+      <div className="col">
+        <Nav Toggle={Toggle} />
+
+       <div>
+          <h4 className="description">Të dhënat për Aeroplanin</h4>
+      <div className="container mt-4">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="id"
+              hidden
+              value={id}
+              onChange={(event) => {
+                setId(event.target.value);
+              }}
+            />
+
+            <label className="label">Kompania</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={kompania}
+              onChange={(event) => {
+                setKompania(event.target.value);
+              }}
+            />
+             <label className="label">Nr i Ulëseve VIP</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={nr_Uleseve_VIP}
+              onChange={(event) => {
+                setNrUleseveVip(event.target.value);
+              }}
+            />
+            <label className="label">Nr i Ulëseve Biznes</label>
+             <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={nr_Uleseve_Biznes}
+              onChange={(event) => {
+                setNrUleseveBiznes(event.target.value);
+              }}
+            />
+            <label className="label">Nr i Ulëseve Ekonomike</label>
+               <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={nr_Uleseve_Ekonomike}
+              onChange={(event) => {
+                setNrUleseveEkonomike(event.target.value);
+              }}
+            />
+          </div>
+
+          <div>
+            <button className="btn btn-success m-4 button" onClick={save}>
+              Save
+            </button>
+            <button className="btn btn-warning m-4 button" onClick={update}>
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+      <br></br>
+
+      {/* Alert Message */}
+      {isAlertVisible && (
+        <div
+          className={`alert ${
+            alertMessage.includes("Error") ? "alert-danger" : "alert-success"
+          }`}
+        >
+          {alertMessage}
+        </div>
+      )}
+
+      <div className="table-responsive m-3">
+        <table className="table border-gray">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Kompania</th>
+              <th scope="col">Nr i Ulëseve VIP</th>
+              <th scope="col">Nr i Ulëseve Biznes</th>
+              <th scope="col">Nr i Ulëseve Ekonomike</th>
+
+              <th scope="col">Opsionet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {aeroplanet.map(function fn(aeroplani) {
+              return (
+                <tr key={aeroplani.id}>
+                  <td>{aeroplani.id}</td>
+                  <td>{aeroplani.kompania}</td>
+                  <td>{aeroplani.nr_Uleseve_VIP}</td>
+                  <td>{aeroplani.nr_Uleseve_Biznes}</td>
+                  <td>{aeroplani.nr_Uleseve_Ekonomike}</td>
+                  <td className="d-flex align-items-center button-container ">
+                    <button
+                      type="button"
+                      className="btn btn-warning mx-1 button"
+                      onClick={() => editAeroplani(aeroplani)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger mx-1 button"
+                      onClick={() => deleteAeroplani(aeroplani.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
   );
 }
 
